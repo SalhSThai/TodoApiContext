756f5a067ffac23f4a3a8f5e10e0d349e9e416a4
SalhSthai <firstheart00@gmail.com>
2022-08-25 13:19:07 +0700

    reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import TodoForm from "./components/TodoForm";
 import TodoContainer from "./components/TodoContainer";
+import { useEffect, useReducer } from "react";
+import axios from 'axios'
+
+function reducer(state, action) {
+  if (action.type === "LOAD_TODO") {
+    return action.payload.todos;
+    // {type: 'CREATE_TODO',payload:{todo: new object todo}}
+  } else if (action.type === "CREATE_TODO") {
+    //  return [newTodo,oldTodo]
+    return [action.payload.todo,...state]
+  }
+}
 
 function App() {
+  const obj = useReducer(reducer, []);// return [currentState,dispatchFunction]
+  //dispatchFunction(actionObj) actionsObj = {type}
+  // Todo WEB App มี  Action ที่อาจจะเกิดขึ้น เช่น Create Todo,Delete Todo,Update Todo
+  const [todos, dispatch] = useReducer(reducer, []);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/todos')
+      .then(res => {
+        dispatch({ type: "LOAD_TODO", payload: { todos: res.data.todos } })
+      }).catch(err => console.log(err))
+
+  }, [])
+const createTodo = (title)=>{
+  axios.post('http://localhost:8080/todos',{title,completed:false})
+  .then(res => {
+    dispatch({type: "CREATE_TODO",payload:{todo:res.data.todo}})
+  }).catch(err => console.log(err))
+
+}
+
+
+
   return (
     <div className="container mt-5 mb-3" style={{ maxWidth: 576 }}>
       <div className="my-4">
-        <TodoForm />
+        <TodoForm onSubmit={createTodo}/>
       </div>
       <TodoContainer />
     </div>
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,22 +1,29 @@
-function TodoForm(props){
-    const {} = props;
+import { useState } from "react";
+
+function TodoForm(props) {
+  const { onSubmit } = props;
+  const [input, setInput] = useState('');
+
+  const handleSubmitform = event=>{
+    event.preventDefault();
+    onSubmit(input);
+    setInput('');
+  }
 
 
 
-
-    
-    return (<form>
-          <div className="input-group">
-            <input type="text" className="form-control" />
-            <button className="btn btn-primary">
-              <i className="fa-solid fa-check" />
-            </button>
-            <button className="btn btn-outline-secondary">
-              <i className="fa-solid fa-xmark" />
-            </button>
-          </div>
-          {/* <small className="text-danger">Title is required.</small> */}
-        </form>)
+  return (<form onSubmit={ handleSubmitform}>
+    <div className="input-group">
+      <input type="text" className="form-control" value={input} onChange={e => setInput(e.target.value)} />
+      <button className="btn btn-primary">
+        <i className="fa-solid fa-check" />
+      </button>
+      <button className="btn btn-outline-secondary">
+        <i className="fa-solid fa-xmark" />
+      </button>
+    </div>
+    {/* <small className="text-danger">Title is required.</small> */}
+  </form>)
 }
 
 export default TodoForm;
